50e765a5c74ec5c1f34a446795411a4e48342e91
Jugal Modi <119117599+jugalmodi0111@users.noreply.github.com>
2025-10-28 21:12:26 +0530

    Remove unnecessary whitespace in Clock Gating module

--- 77_Clock Gating/Clock Gating.v
+++ 77_Clock Gating/Clock Gating.v
@@ -1,14 +1,8 @@
 module clock_gating (
   input wire clk, 
-  
   input wire enable, 
-  
   output wire gated_clk
-  
 ) ; 
   
-  
-  
-  assign gated_clk = clk & enable; 
-  
+assign gated_clk = clk & enable; 
 endmodule
