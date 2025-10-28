f99200abf6b88ba6d62254fac0b07563183dbf3c
Jugal Modi <119117599+jugalmodi0111@users.noreply.github.com>
2025-10-28 21:13:34 +0530

    Update Clock Gating.v

--- 77_Clock Gating/Clock Gating.v
+++ 77_Clock Gating/Clock Gating.v
@@ -1,14 +1,9 @@
-module clock_gating (
+module clock_gating 
+  (
   input wire clk, 
-  
   input wire enable, 
-  
-  output wire gated_clk
-  
-) ; 
-  
-  
+  output wire gated_clk 
+  ); 
   
   assign gated_clk = clk & enable; 
-  
 endmodule
